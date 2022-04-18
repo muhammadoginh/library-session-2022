2e661b31b353eebea13b63542d7092e8af648e41
muhammadoginh <moginh@gmail.com>
2022-04-18 21:51:54 +0700

    repository Python DTS PRoA 2022 Library session 1-5

---
+++ Library Session 1 Tkinter/section1.py
@@ -0,0 +1,6 @@
+#Creating a window
+
+import tkinter as tk
+
+root = tk.Tk()
+root.mainloop()
---
+++ Library Session 2 Flask/flask-gentelella/app/base/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+
+## login and registration
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', id='username_login')
+    password = PasswordField('Password', id='pwd_login')
+
+
+class CreateAccountForm(FlaskForm):
+    username = StringField('Username', id='username_create')
+    email = StringField('Email')
+    hp = StringField('Phone')
+    password = PasswordField('Password', id='pwd_create')
